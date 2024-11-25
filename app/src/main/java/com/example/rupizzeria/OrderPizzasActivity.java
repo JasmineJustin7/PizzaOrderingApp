39b0ccf13533d6e3a25e85e46c961a828bb748a7
Jasmine Justin <jasminejustin7@gmail.com>
2024-11-25 13:30:37 -0500

    order_pizza user interface developed

--- app/src/main/java/com/example/rupizzeria/OrderPizzasActivity.java
+++ app/src/main/java/com/example/rupizzeria/OrderPizzasActivity.java
@@ -4,14 +4,58 @@ package com.example.rupizzeria;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import Classes.Topping;
 
 
 public class OrderPizzasActivity extends AppCompatActivity {
 
+    //Declare an instance of ArrayList to hold the items to be display with the RecyclerView
+    private ArrayList<Topping> toppings = new ArrayList<>();
+    /* All the images associated with the menu items are stored in the res/drawable folder
+     *  Each image are accessed with the resource ID, which is an integer.
+     *  We need an array of integers to hold the resource IDs. Make sure the index of a given
+     *  ID is consistent with the index of the associated menu item in the ArrayList.
+     *  An image resource could also be an URI.
+     */
+    /*private int [] toppingImages = {R.drawable.apple, R.drawable.banana, R.drawable.grapes,
+            R.drawable.mango, R.drawable.orange, R.drawable.peach, R.drawable.pineapple,
+            R.drawable.strawberry};*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_pizza);
+        RecyclerView recyclerViewc = findViewById(R.id.recycler_toppings);
+        setupMenuItems(); //add the list of items to the ArrayList
+        ItemsAdapter adapter = new ItemsAdapter(this, toppings); //create the adapter
+        recyclerViewc.setAdapter(adapter); //bind the list of items to the RecyclerView
+        //use the LinearLayout for the RecyclerView
+        recyclerViewc.setLayoutManager(new LinearLayoutManager(this));
     }
+
+
+    /**
+     * Helper method to set up the data (the Model of the MVC).
+     */
+    private void setupMenuItems() {
+        /*
+         * Item names are defined in a String array under res/string.xml.
+         * Your item names might come from other places, such as an external file, or the database
+         * from the backend.
+         */
+        String [] itemNames = getResources().getStringArray(R.array.itemNames);
+        /* Add the items to the ArrayList.
+         * Note that I use hardcoded prices for demo purpose. This should be replace by other
+         * data sources.
+         */
+        for (int i = 0; i < itemNames.length; i++) {
+            //toppings.add(new Items(itemNames[i], itemImages[i], "$1.39"));
+        }
+    }
+
 }
